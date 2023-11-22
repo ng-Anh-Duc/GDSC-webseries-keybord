cb39186b4bc45594f8ecaf383781bbb864bd6443
maiyume <kietpg@vng.comvn>
2023-11-22 20:00:30 +0700

    modified:   js/script.js 	modified:   style/style.css

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-/* gọi api từ nguồn khác lấy 50 từ đơn giản hơn, chia từng từ thành 1 <char> */
-const typingText = document.querySelector("#text-box p");
-async function getRandomParagraph() {
-      const response = await fetch('https://random-word-api.herokuapp.com/word?number=50&length=5');
-      const data = await response.json();
-      const randomParagraph = data.join(' ');
-      console.log(randomParagraph);
-      typingText.innerHTML = "";
-      randomParagraph.split("").forEach(char => {
-        let span = `<span>${char}</span>`
-        typingText.innerHTML += span;
-    });
-    typingText.querySelectorAll("span")[0].classList.add("active");
-}
-document.addEventListener('DOMContentLoaded', getRandomParagraph);
+
 /* display key pressed */
 
 
@@ -30,11 +16,12 @@ let correctWordsElement = document.getElementById('correct-words');
 
 async function getRandomParagraph() {
     try {
-        const response = await fetch('https://baconipsum.com/api/?type=meat-and-filler&paras=1');
+        const response = await fetch('https://random-word-api.herokuapp.com/word?number=50&length=5');
         const data = await response.json();
 
         if (Array.isArray(data) && data.length > 0) {
-            originalText = data[0];
+            
+            originalText = data.join(' ');
             randomText.innerHTML = originalText.split('').map(char => `<span>${char}</span>`).join('');
         } else {
             console.error('Invalid response from the API');
